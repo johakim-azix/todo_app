240901beb4e6f56b985c78ae60be8f2d7bd00e57
penda joseph <johakimazix4@gmail.com>
2022-01-23 01:10:03 +0100

    building taskItemViewHolder, adding data.json for fake data and feeding the task list view

--- js/index.js
+++ js/index.js
@@ -1,10 +1,52 @@
 
 let currentlyEditedItem = null
+let tasksQueue = null
+
+class TodoRepository{
+    constructor() {
+        this.db = this.getDbInstance()
+    }
+
+    getDbInstance(){
+        // try{
+        //     let dbInstance = indexedDB.open("tasks")
+        //         dbInstance.onupgradeneeded
+        // }catch (exception) {
+        //
+        // }
+
+    }
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    fetch("data.json")
+        .then(response => response.text())
+        .then(data => {
+            data = JSON.parse(data)
+            tasksQueue = new TasksQueue(document.getElementsByClassName("queue")[0], data.tasks)
+        })
 
+
+    let mainSceneLayout = document.getElementsByClassName("main-scene")[0]
+    let mainContentLayout = document.getElementsByClassName("main-content")[0]
+
+    mainSceneLayout.addEventListener("onscroll", collapseAppBar())
+    mainContentLayout.addEventListener("onscroll", collapseAppBar())
 })
 
+
+function collapseAppBar() {
+    console.log(this)
+}
+
+function onCreateTaskIntent(element) {
+    console.log("ddddddddddddd")
+}
+
+function submitTask(button) {
+    pre
+    console.log("geeddd")
+}
 
 function toggleTodoState(check) {
     // alert("here : "+getTodoItemView(check).innerHTML)
@@ -28,7 +70,6 @@ function toggleDetails(button) {
     enableBtn(todoItem.getElementsByTagName('button'))
 }
 
-
 function toggleItemsChecks(currentCheck) {
     let checks = document.getElementsByName("todo-details-toggle")
     checks.forEach(check =>  {
@@ -39,8 +80,6 @@ function toggleItemsChecks(currentCheck) {
         }
     })
 }
-
-
 
 function saveChanges(button){
     let todoItem = document.getElementById(getTodoItemViewId(button))
@@ -135,25 +174,85 @@ function getTodoItemViewId(button) {
     return todoItemViewId
 }
 
-class TodoQueue {
-    constructor(queueLayout, todoArray) {
+class TasksQueue {
+    constructor(queueLayout, tasks) {
         this.layout = queueLayout
-        this.todoArray = todoArray
+        this.tasks = tasks
+        this.renderTasks()
     }
 
-    getTodo(index){
-        return this.todoArray[index]
+    renderTasks(){
+        this.tasks.forEach((task, index) => {
+            this.layout.appendChild(new TasksItemViewHolder().bindViewHolder(task, index))
+        })
     }
 
-    setTodo(todoObj){
-        this.todoArray.push(todoObj)
+
+    getTask(index){
+        return this.tasks[index]
     }
 
-    toggleTodoState(index){
-        this.todoArray[index].isCompleted = !(this.todoArray[index].isCompleted)
+    setTask(todoObj){
+        this.tasks.push(todoObj)
+    }
+
+    toggleTaskState(index){
+        this.tasks[index].isCompleted = !(this.tasks[index].isCompleted)
         return true
     }
 
+}
+
+class TasksItemViewHolder {
+    static TAKS_ITEM_VIEW_HOLDER_TEMPLATE = "<div id=\"task-item-id\" class=\"todo\">\n" +
+        "                    <input name=\"todo-details-toggle\" type=\"checkbox\">\n" +
+        "                    <input name=\"content-editable-toggle\" type=\"checkbox\">\n" +
+        "                    <div class=\"title-container\">\n" +
+        "                        <div onclick=\"toggleTodoState(this)\" class=\"radio\">\n" +
+        "                            <img src=\"images/icon-check.svg\" alt=\"\">\n" +
+        "                        </div>\n" +
+        "                        <div class=\"title\"  style=\"margin-right: 0!important\">\n" +
+        "                            Finir de designer cette vue ...\n" +
+        "                        </div>\n" +
+        "                        <button onclick=\"toggleDetails(this)\" class=\"btn toggle-details\">\n" +
+        "                            <i class=\"fa fa-caret-down\"></i>\n" +
+        "                        </button>\n" +
+        "                    </div>\n" +
+        "                    <div class=\"description-container\" >\n" +
+        "                        Fondre la page et la rendre pleinement responsive pour les divers support.\n" +
+        "                    </div>\n" +
+        "                    <div class=\"controls\">\n" +
+        "                        <span></span>\n" +
+        "                        <div>\n" +
+        "                            <button onclick=\"saveChanges(this)\" class=\"btn save\">\n" +
+        "                                <i class=\"fa fa-save\"></i> Save\n" +
+        "                            </button>\n" +
+        "                            <button onclick=\"editTodo(this)\" class=\"btn save\">\n" +
+        "                                <i class=\"fa fa-pen\"></i> Edit\n" +
+        "                            </button>\n" +
+        "                            <button onclick=\"deleteTodo(this)\" class=\"btn delete\">\n" +
+        "                                <i class=\"fa fa-trash\"></i> Delete\n" +
+        "                            </button>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>"
+
+    constructor() {
+        this.viewHolder = this.createViewHolder()
+    }
+
+    createViewHolder(){
+        let parser = new DOMParser()
+        return  parser.parseFromString(TasksItemViewHolder.TAKS_ITEM_VIEW_HOLDER_TEMPLATE, "text/html").getElementById("task-item-id")
+    }
+
+    bindViewHolder(task, taskIndex){
+        this.viewHolder.setAttribute('id', "task-item-"+taskIndex)
+        if (task.isCompleted) this.viewHolder.getElementsByClassName("radio")[0].style = "background-color: black; transition: all ease-in-out .15s;"
+        this.viewHolder.getElementsByClassName("title")[0].innerText = task.title
+        this.viewHolder.getElementsByClassName("description-container")[0].innerText = task.description
+        return this.viewHolder
+    }
 }
 
 class Todo {
